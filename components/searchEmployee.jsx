5a4f77e89812aead3b22c5a94fe6f27e14a359a9
dagreatcode <dagreatcode@gmail.com>
2020-10-31 22:37:06 -0400

    fixing employee first

--- components/searchEmployee.jsx
+++ components/searchEmployee.jsx
@@ -1,17 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const searchEmployee = () => {
-    return (
-        <div>
-           <div className="container">
-               <div className="row">
-                  <div className="col-sm-2" />
-                  <div className="col-sm-8" />
-                    <h1>My search bar go here</h1>
-               </div>
-           </div> 
-        </div>
-    );
-};
+class searchEmployee extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
 
 export default searchEmployee;
