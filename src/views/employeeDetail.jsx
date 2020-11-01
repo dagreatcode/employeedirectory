4f089efe19de5f5aeb4b707ddfc4264dd792a453
dagreatcode <dagreatcode@gmail.com>
2020-10-31 21:37:33 -0400

    working on the employee api call and state

---
+++ src/views/employeeDetail.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import axios from "axios";
+import searchEmployee from "../components/searchEmployee";
+import API from "../utils/API";
+
+class employeeDetail extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default employeeDetail;
