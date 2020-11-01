42d160fc018f5e214de3f6b6a8d8bfa1aecb0341
dagreatcode <dagreatcode@gmail.com>
2020-10-31 22:26:33 -0400

    fixing search

---
+++ components/searchEmployee.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const searchEmployee = () => {
+    return (
+        <div>
+           <div className="container">
+               <div className="row">
+                  <div className="col-sm-2" />
+                  <div className="col-sm-8" />
+                    <h1>My search bar go here</h1>
+               </div>
+           </div> 
+        </div>
+    );
+};
+
+export default searchEmployee;
