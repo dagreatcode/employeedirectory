e820c0cf5a42058c6a0cefa9d8bbe953a8560a38
dagreatcode <dagreatcode@gmail.com>
2020-10-31 21:28:31 -0400

    fixing up

---
+++ src/components/searchEmployee.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import API from "../utils/API";
+import employeeDetail from "./views/employeeDetail";
+import "../App.css";
+
+class searchEmployee extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default searchEmployee;
